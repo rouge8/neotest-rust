6cf72cadcd8c885e1bfeee8c9b7b9960e77ec511
Andrew Bell <andrew.bell012276@gmail.com>
2022-12-16 12:38:07 +0000

    fix tests requiring async to run correctly

--- tests/init_spec.lua
+++ tests/init_spec.lua
@@ -3,15 +3,15 @@ local plugin = require("neotest-rust")
 local Tree = require("neotest.types").Tree
 
 describe("is_test_file", function()
-    it("matches Rust files with tests in them", function()
+    async.it("matches Rust files with tests in them", function()
         assert.equals(true, plugin.is_test_file(vim.loop.cwd() .. "/tests/data/src/mymod/foo.rs"))
     end)
 
-    it("doesn't discover non-Rust files", function()
+    async.it("doesn't discover non-Rust files", function()
         assert.equals(false, plugin.is_test_file(vim.loop.cwd() .. "/tests/data/Cargo.toml"))
     end)
 
-    it("doesn't discover Rust file without tests in it", function()
+    async.it("doesn't discover Rust file without tests in it", function()
         assert.equals(false, plugin.is_test_file(vim.loop.cwd() .. "/tests/data/src/mymod/notests.rs"))
     end)
 end)
